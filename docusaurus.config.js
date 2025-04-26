7e2e52d6b4e12fd897ca76aa164a1b7c9d8475de
algotao <algotao@gmail.com>
2025-04-26 10:29:19 +0800

    4月更新

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Raspberry Pi 树莓派 (官网25年2月更新)',
+  title: 'Raspberry Pi 树莓派 (官网25年4月更新)',
   tagline: '中文版文档',
   favicon: 'img/pi.svg',
 
